0dd165f024d0a3087b20633eede14b8c499b9637
a0x8o <a0x8o>
2023-04-12 14:48:31 +0000

    Updates to references

--- 00_digitization_context.py
+++ 00_digitization_context.py
@@ -11,6 +11,7 @@
 # MAGIC 
 # MAGIC + antoine.amend@databricks.com
 # MAGIC + eon.retief@databricks.com
+# MAGIC + alex.barreto@datatbricks.com
 
 # COMMAND ----------
 
@@ -20,7 +21,7 @@
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC &copy; 2022 Databricks, Inc. All rights reserved. The source in this notebook is provided subject to the Databricks License [https://databricks.com/db-license-source].  All included or referenced third party libraries are subject to the licenses set forth below.
+# MAGIC &copy; 2023 Databricks, Inc. All rights reserved. The source in this notebook is provided subject to the Databricks License [https://databricks.com/db-license-source].  All included or referenced third party libraries are subject to the licenses set forth below.
 # MAGIC 
 # MAGIC | library                                | description             | license    | source                                              |
 # MAGIC |----------------------------------------|-------------------------|------------|-----------------------------------------------------|
